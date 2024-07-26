e4827f02a29d7a402877efa7a5240825843b95ab
rjm <jeanmichelrochoy62@gmail.com>
2024-07-26 19:07:19 +0200

    début retouche du formulaire

--- src/pages/Logistique/Logistique.jsx
+++ src/pages/Logistique/Logistique.jsx
@@ -1,9 +1,19 @@
+import {useContext, useEffect, useState} from 'react'
+import {DataContext} from '../../utils/Context/DataContext'
 import logistique1 from '../../assets/img/logistique1.png'
 import logistique2 from '../../assets/img/logistique2.png'
 import logistique3 from '../../assets/img/logistique3.jpg'
+import Contact from '../Contact/Contact'
 import '../../style/Logistique.css'
 
 function Logistique() {
+    const {contactButton, setContactButton} = useContext(DataContext)
+    const [display, setDisplay] = useState("OFF")
+ 
+useEffect(() => {
+    contactButton ===true ? setDisplay("ON") : setDisplay("OFF")
+}, [contactButton]) 
+
     return (
         <div className="logistiqueContainer">
             <figure>
@@ -18,6 +28,7 @@ function Logistique() {
                 <img src={logistique3} alt="" />
                 <figcaption>Préparation de commandes</figcaption>
             </figure>
+            {display=="ON" ? <Contact /> : ("")}
         </div>
     )
 }
--- src/pages/Stockage/Stockage.jsx
+++ src/pages/Stockage/Stockage.jsx
@@ -1,8 +1,20 @@
+import {useContext, useEffect, useState} from 'react'
+import {DataContext} from '../../utils/Context/DataContext'
 import photoPageStockage from '../../assets/img/stockage3.png'
+import Contact from '../Contact/Contact'
 import  '../../style/Stockage.css'
 
 function Stockage() {
+    const {contactButton, setContactButton} = useContext(DataContext)
+    const [display, setDisplay] = useState("OFF")
+ 
+useEffect(() => {
+    contactButton ===true ? setDisplay("ON") : setDisplay("OFF")
+}, [contactButton]) 
+
+
     return (
+        <>
         <div className="stockageContainer"> 
              <img src={photoPageStockage} alt= "ttt" />
              <figcaption>
@@ -14,8 +26,10 @@ function Stockage() {
                 <p>Surveillance du site 24H/24H</p>
                 <p>Zone entièrement fermée</p>
             </figcaption>
-
+            {display=="ON" ? <Contact />: ("")}
         </div>
+        {/* {display=="ON" ? <div className='milou'></div>: ("")}  */}
+        </>
     )
 }
 

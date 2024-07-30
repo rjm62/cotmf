3b0dff7d2a3ccb335b8ca85135322644303edaa8
rjm <jeanmichelrochoy62@gmail.com>
2024-07-30 18:08:28 +0200

    essai 14 deploy

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
                 <Header />
                     <Routes>
                         <Route path="/" element={<Navigate replace to="/cotmf"/>} />
-                        <Route path="/" element={<Accueil />} />
-                        <Route path="/Stockage" element={<Stockage />} />
-                        <Route path="/Transport" element={<Transport />} />
-                        <Route path="/Logistique" element={<Logistique />} />
+                        <Route path="/cotmf" element={<Accueil />} />
+                        <Route path="/cotmf/Stockage" element={<Stockage />} />
+                        <Route path="/cotmf/Transport" element={<Transport />} />
+                        <Route path="cotmf//Logistique" element={<Logistique />} />
                     </Routes>
                 <Footer />
             </DataContextProvider>

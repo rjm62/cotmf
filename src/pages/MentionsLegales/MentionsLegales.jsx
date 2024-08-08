35fc2ae3f19b14e75e4823087926fc93d015c7bc
rjm <jeanmichelrochoy62@gmail.com>
2024-08-08 12:20:53 +0200

    installation du scroller sur pages du footer

--- src/pages/MentionsLegales/MentionsLegales.jsx
+++ src/pages/MentionsLegales/MentionsLegales.jsx
@@ -14,14 +14,14 @@ function MentionsLegales() {
 
     return (
         <div className="mentionsLegalesContainer">
-            <h2>MENTIONS LEGALES</h2>
-            <p className='center'>En vigueur au 15 août 2024</p>
-            <p></p>
-            <p>Conformément aux dispositions de la loi n°2004-575 du 21 juin 2004 pour la
-                confiance en l'économie numérique, il est porté à la connaissance des
-                utilisateurs et visiteurs, ci-après l' "utilisateur", du site Co.TMF,
-                ci-après le "site", les présentes mentions légales.</p>
-                <p>La connexion et la navigation sur le Site par l'Utilisateur implique
+            <div className='scroller'>
+                <h2>MENTIONS LEGALES</h2>
+                <p className='center'>En vigueur au 08 août 2024</p>
+                <p></p>
+                <p>Conformément aux dispositions de la loi n°2004-575 du 21 juin 2004 pour la
+                    confiance en l'économie numérique, il est porté à la connaissance des
+                    utilisateurs et visiteurs du site Co.TMF, les présentes mentions légales.</p>
+                <p>La connexion et la navigation sur le Site par l'utilisateur implique
                 l'acceptation intégrale et sans réserve des présentes mentions légales.
                 </p>
                 <p></p>
@@ -45,10 +45,10 @@ function MentionsLegales() {
                 <p></p>
                 <h3>ACCES AU SITE</h3>
                 <p>Le site est normalement accessible à tout moment pour l'utilisateur.
-                   Toutefois, l'éditeur pourra à tout moment suspendre, limiter ou interrompre
-                   le site afin de procéder, notamment, à des mises à jour ou des modifications 
-                   de son contenu.
-                   L'éditeur ne pourra en aucun cas être tenu responsable des conséquences éventuelles
+                    Toutefois, l'éditeur pourra à tout moment suspendre, limiter ou interrompre
+                    le site afin de procéder, notamment, à des mises à jour ou des modifications 
+                    de son contenu.
+                    L'éditeur ne pourra en aucun cas être tenu responsable des conséquences éventuelles
                     de cette indisponibilité sur les activités de l'utilisateur.</p>
                     <p></p>
                     <p>Toute utilisation, reproduction, diffusion, commercialisation, modifications
@@ -56,6 +56,7 @@ function MentionsLegales() {
                     pourra entraîner des actions et poursuites judiciaires telles que prévues par la 
                     réglementation en vigueur.
                     </p>
+                </div>
                 {display=="ON" ? <Contact />: ("")}
         </div>
     )
